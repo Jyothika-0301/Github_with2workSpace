6d16fd84c797e4f6fbdb692c009147ea76ff15db
jyothika <jyothika@DESKTOP-RPDOIFA>
2026-09-02 17:46:35 +0530

    first commit

---
+++ Github_with2workspace/src/main/java/sample/Demo.java
@@ -0,0 +1,9 @@
+package sample;
+
+import java.security.DomainCombiner;
+
+public class Demo {
+public static void main(String[] args) {
+	System.out.println("hi");
+}
+}
---
+++ Github_with2workspace/src/test/java/Sampletest/DemoTest.java
@@ -0,0 +1,7 @@
+package Sampletest;
+
+public class DemoTest {
+public static void main(String[] args) {
+	System.out.println("jkgb");
+}
+}
